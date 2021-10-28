66c3dfc6ecd9e597ddb2c2bc0271856cc470360f
Polina <polina17012001@mail.ru>
2021-10-29 00:58:30 +0300

    add version1

---
+++ class-based-component/src/components/ShopItemClass.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types'
+import ItemModel from '../models/ItemModel';
+import React from 'react'
+
+class ShopItemClass extends React.Component {
+
+    render() {
+        const {item} = this.props
+        return(
+            <div class="main-content">
+                <h2>{item.brand}</h2>
+                <h1>{item.title}</h1>
+                <h3>{item.description}</h3>
+                <div className="description">{item.descriptionFull}</div>
+                <div className="highlight-window mobile"><div class="highlight-overlay"></div></div>
+                <div className="divider"></div>
+                <div className="purchase-info">
+                    <div className="price">{item.currency}{item.price}.00</div>
+                    <button>Добавить в корзину</button>
+                </div>
+            </div>
+        )
+    }
+
+
+}
+
+ShopItemClass.propTypes = {
+    item: PropTypes.instanceOf(ItemModel).isRequired
+}
+
+export default ShopItemClass;
